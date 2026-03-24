1428875c517660c143dc2366fac14d60e7948800
Your Name <you@example.com>
2026-03-24 23:57:19 +0900

    add devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,3 +5,7 @@ class ApplicationController < ActionController::Base
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
 end
+
+def after_sign_in_path_for(resource)
+  root_path
+end
