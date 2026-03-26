6dc065869b1f12e027fc7b56f4a056f31f13b82d
Your Name <you@example.com>
2026-03-27 00:43:23 +0900

    add first-login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :trackable
+
+  def first_login?
+    sign_in_count <= 1
+  end
 end
