fd1a31c6ad59b60fa327fc1d73e904fb91d411b6
Your Name <you@example.com>
2026-04-04 16:33:46 +0900

    add category-model

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -1,5 +1,6 @@
 class Decision < ApplicationRecord
   belongs_to :user
+  belongs_to :category
 
   validates :title, presence: true, length: { maximum: 100 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_28_072606) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_04_070824) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "decisions", force: :cascade do |t|
+    t.bigint "category_id"
     t.datetime "created_at", null: false
     t.text "description"
     t.string "title"
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.index ["category_id"], name: "index_decisions_on_category_id"
     t.index ["user_id"], name: "index_decisions_on_user_id"
   end
 
@@ -41,5 +49,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_28_072606) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "decisions", "categories"
   add_foreign_key "decisions", "users"
 end
