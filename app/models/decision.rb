eb527f9828ca053096d5d7d0e887ca2d572d1db1
Your Name <you@example.com>
2026-04-05 21:29:18 +0900

    add category-select

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -3,4 +3,5 @@ class Decision < ApplicationRecord
   belongs_to :category, optional: true
 
   validates :title, presence: true, length: { maximum: 100 }
+  validates :category_id, presence: true
 end
