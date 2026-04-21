99f5fa7c390a9d1d40a9b464c040465e28dcd363
Your Name <you@example.com>
2026-04-21 21:52:08 +0900

    add option-validation

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,5 +1,6 @@
 class Option < ApplicationRecord
   belongs_to :decision
 
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum: 100 }
+  validates :content, uniqueness: { scope: :decision_id }
 end
