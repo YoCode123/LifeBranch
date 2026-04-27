7c74da52ebbb64843b59267bc5b9b51b978932a4
Your Name <you@example.com>
2026-04-27 23:30:52 +0900

    add date-to-decision-emotions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_25_104635) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_27_134204) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -24,6 +24,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_25_104635) do
     t.datetime "created_at", null: false
     t.bigint "decision_id", null: false
     t.bigint "emotion_type_id", null: false
+    t.date "recorded_on"
     t.datetime "updated_at", null: false
     t.index ["decision_id"], name: "index_decision_emotions_on_decision_id"
     t.index ["emotion_type_id"], name: "index_decision_emotions_on_emotion_type_id"
