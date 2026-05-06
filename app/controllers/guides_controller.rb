9818e717ac8a54d6c5d0cbd8a2d408d216b84b3c
Your Name <you@example.com>
2026-05-06 22:22:53 +0900

    add index-ui-improvement

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,4 +1,11 @@
 class GuidesController < ApplicationController
+  before_action :authenticate_user!
+
   def show
+  end
+
+  def complete
+    current_user.update(first_login_done: true)
+    redirect_to new_decision_path
   end
 end
