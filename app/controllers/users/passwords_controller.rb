69aadc811a48890dcf1b54b8fc617baa7a98974a
Your Name <you@example.com>
2026-05-17 21:27:02 +0900

    fix password確認

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,9 +1,4 @@
 class Users::PasswordsController < Devise::PasswordsController
   def create
-    super do |resource|
-      if successfully_sent?(resource)
-        redirect_to password_sent_path and return
-      end
-    end
-  end
+  redirect_to root_path
 end
