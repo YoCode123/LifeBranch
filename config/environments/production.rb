e75060837aafee3cfa2dfdd05f1f9140f6b94dfa
Your Name <you@example.com>
2026-05-17 23:54:34 +0900

    fix メール送信設定20260517

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,11 +86,12 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: "gmail.com",
   user_name: ENV["SMTP_USER_NAME"],
   password: ENV["SMTP_PASSWORD"],
   authentication: :plain,
-  enable_starttls_auto: true
+  enable_starttls_auto: true,
+  open_timeout: 10,
+  read_timeout: 10
 }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
