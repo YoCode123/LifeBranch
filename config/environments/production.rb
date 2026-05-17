3418c9b773aeec3c0e37d3978846174f0bcd767a
Your Name <you@example.com>
2026-05-17 23:01:09 +0900

    fix メール送信設定0517

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,7 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
+  domain: "gmail.com",
   user_name: ENV["SMTP_USER_NAME"],
   password: ENV["SMTP_PASSWORD"],
   authentication: :plain,
