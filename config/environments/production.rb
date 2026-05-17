68e15854854ba1b46f97606178d7dd8618f0f10e
Your Name <you@example.com>
2026-05-18 00:01:14 +0900

    fix メール送信テスト7

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,8 +82,8 @@ config.public_file_server.headers = {
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
 
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
+config.action_mailer.delivery_method = :test
+# config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
   user_name: ENV["SMTP_USER_NAME"],
@@ -92,7 +92,7 @@ config.action_mailer.smtp_settings = {
   enable_starttls_auto: true,
   open_timeout: 10,
   read_timeout: 10
-}
+#
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
