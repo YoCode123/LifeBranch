4f522587dfeb8449205e4c6a26cd0a29b17c13e4
Your Name <you@example.com>
2026-05-20 01:20:42 +0900

    fix devise sessions and mailer

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -2,9 +2,16 @@ class Users::PasswordsController < Devise::PasswordsController
   def create
     self.resource = resource_class.send_reset_password_instructions(resource_params)
 
-    redirect_to password_sent_path
+    if successfully_sent?(resource)
+      redirect_to password_sent_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def sent
+  end
+
+  def changed
   end
 end
