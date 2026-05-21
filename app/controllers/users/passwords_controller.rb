fc83d24b57dbc1d619b5e0e5b90c52b03b0b9a7a
Your Name <you@example.com>
2026-05-21 22:19:59 +0900

    add password changed page

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -3,10 +3,27 @@ class Users::PasswordsController < Devise::PasswordsController
     self.resource = resource_class.send_reset_password_instructions(resource_params)
 
     if successfully_sent?(resource)
-      flash.discard(:notice)
       redirect_to password_sent_path
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    self.resource = resource_class.reset_password_by_token(resource_params)
+
+    if resource.errors.empty?
+      resource.unlock_access! if unlockable?(resource)
+
+      if Devise.sign_in_after_reset_password
+        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+        set_flash_message!(:notice, flash_message)
+        sign_in(resource_name, resource)
+      end
+
+      redirect_to password_changed_path
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
