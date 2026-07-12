7e828d74a183ab4c6a90eb8aaf1c36f8872296c2
Your Name <you@example.com>
2026-07-12 23:17:09 +0900

    Add category partial search suggestions

--- app/javascript/controllers/category_suggestions_controller.js
+++ app/javascript/controllers/category_suggestions_controller.js
@@ -1,23 +1,49 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["input", "hidden"]
-
-  select() {
-    const value = this.inputTarget.value
-
-    const option = this.categories.find(
-      category => category.name === value
-    )
-
-    if (option) {
-      this.hiddenTarget.value = option.id
-    }
-  }
+  static targets = ["input", "list", "hidden"]
 
   connect() {
-    this.categories = JSON.parse(
-      this.element.dataset.categories
-    )
+    document.addEventListener("click", this.close.bind(this))
+  }
+
+  disconnect() {
+    document.removeEventListener("click", this.close.bind(this))
+  }
+
+  search() {
+    const keyword = this.inputTarget.value
+
+    fetch(`/categories/search?keyword=${encodeURIComponent(keyword)}`)
+      .then(response => response.json())
+      .then(categories => {
+        this.listTarget.innerHTML = ""
+
+        categories.forEach(category => {
+          const button = document.createElement("button")
+
+          button.type = "button"
+          button.className =
+            "list-group-item list-group-item-action"
+
+          button.textContent = category.name
+
+          button.addEventListener("click", (event) => {
+            event.stopPropagation()
+
+            this.inputTarget.value = category.name
+            this.hiddenTarget.value = category.id
+            this.listTarget.innerHTML = ""
+          })
+
+          this.listTarget.appendChild(button)
+        })
+      })
+  }
+
+  close(event) {
+    if (!this.element.contains(event.target)) {
+      this.listTarget.innerHTML = ""
+    }
   }
 }
