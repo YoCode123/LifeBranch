dbac8e679af494fa5b9ecaf3de4bbe9c51725ff4
Your Name <you@example.com>
2026-07-20 16:00:41 +0900

    feat: add emotion summary and monthly emotion chart

--- app/controllers/emotion_analysis_controller.rb
+++ app/controllers/emotion_analysis_controller.rb
@@ -9,5 +9,11 @@ class EmotionAnalysisController < ApplicationController
 
     @total_emotions = @emotion_counts.values.sum
     @most_emotion = @emotion_counts.max_by { |_, count| count }
+
+    @monthly_emotions = current_user.decisions
+                                    .joins(:emotion_types)
+                                    .where.not(recorded_on: nil)
+                                    .group_by_month(:recorded_on, format: "%Y-%m")
+                                    .count
   end
 end
