f76cec485fdd22f1f887be6950dcbc54e62c6ffb
Your Name <you@example.com>
2026-07-20 15:02:33 +0900

    Add emotion analysis page with Chartkick

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,15 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @decisions =
       current_user.decisions
                   .order(Arel.sql("COALESCE(recorded_on, created_at) DESC"))
+
+    @emotion_chart =
+      current_user.decisions
+                  .joins(:emotion_types)
+                  .group("emotion_types.name")
+                  .count
   end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,10 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.0/dist/js/bootstrap.esm.js"
+
+pin "chartkick"
+pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.5.1/dist/chart.js"
+pin "chart.js/auto", to: "https://ga.jspm.io/npm:chart.js@4.5.1/auto/auto.js"
+pin "@kurkle/color", to: "https://ga.jspm.io/npm:@kurkle/color@0.3.4/dist/color.esm.js"
